e6766fd919b11a77de01b0ca9a6fa31ea7135916
stevanililyanis <stevanililyanisaputri@gmail.com>
2020-12-07 22:18:58 +0700

    Pdf done

--- src/main.js
+++ src/main.js
@@ -4,18 +4,23 @@ import Vue from 'vue'
 import './plugins/bootstrap-vue'
 import App from './App.vue'
 import router from './router'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import axios from 'axios'
+
 import { jsPDF } from "jspdf";
 import 'jspdf-autotable';
 
-Vue.config.productionTip = false
 
+Vue.prototype.$http = axios;
+Vue.prototype.$api = 'http://127.0.0.1:8000/api';
+
+Vue.config.productionTip = false
 Vue.use(jsPDF)
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
 // Install BootstrapVue
 Vue.use(BootstrapVue)
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
-
 
 new Vue({
   router,
